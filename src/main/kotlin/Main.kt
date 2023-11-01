56f6e787c70d4e915a1e4f0cb98b7c54be84a4da
Jwhyee <kawnsdud@naver.com>
2023-11-01 21:56:07 +0900

    add : 타입추론과 함수

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,5 @@
 fun main(args: Array<String>) {
-    var intArr = arrayOf(1, 2, 3, 4, 5)
-
-    var nullArr = arrayOfNulls<Int>(5)
-
-    intArr[2] = 8
-
-    println(intArr[2])
+    println(add(5, 6, 7))
 }
+
+fun add(a: Int, b: Int, c: Int) = a + b + c
