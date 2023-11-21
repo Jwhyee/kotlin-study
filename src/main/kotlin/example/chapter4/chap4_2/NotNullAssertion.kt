8fc5680092dcc67ca9ce6e7ecce10dc346cb806d
Jwhyee <kawnsdud@naver.com>
2023-11-21 21:11:20 +0900

    add : nullable

---
+++ src/main/kotlin/example/chapter4/chap4_2/NotNullAssertion.kt
@@ -0,0 +1,19 @@
+package example.chapter4.chap4_2
+
+class Name(val firstName: String, val familyName: String?)
+class Person(val name: Name?) {
+    fun introduce(): String {
+        val currentName = name ?: return "Unknown"
+        return "${currentName.firstName} ${currentName.familyName}"
+    }
+}
+
+fun main() {
+    sayHello(null)
+    println(Person(Name("John", "Doe")).introduce())
+    println(Person(null).introduce())
+}
+
+fun sayHello(name: String?) {
+    println("Hello, " + (name ?: "Unknown"))
+}
---
+++ src/main/kotlin/example/chapter4/chap4_2/NullableTest.kt
@@ -0,0 +1,40 @@
+package example.chapter4.chap4_2
+
+fun main() {
+
+
+    println(isLetterString("abc"))
+    // String?를 사용하지 않을 경우 에러 발생
+    // error : Null can not be a value of a non-null type String
+    println(isLetterString(null))
+
+    println(isBooleanString("true"))
+    println(isBooleanString(null))
+
+    println(describeNumber(null))
+
+    val n = readLine()!!.toInt()
+    println("n = ${n}")
+
+}
+
+fun isLetterString(s: String?): Boolean {
+    if(s == null) return false
+
+    if(s.isEmpty()) return false
+
+    for(ch in s) {
+        if(!ch.isLetter()) return false
+    }
+
+    return true
+}
+
+fun isBooleanString(s: String?) = s == "false" || s == "true"
+
+fun describeNumber(n: Int?) = when(n) {
+    null -> "null"
+    in 0 .. 10 -> "small"
+    in 11 .. 100 -> "large"
+    else -> "out of range"
+}
